cc3b9c99dba5d1d2f668f9e859cab5795765fca3
bhoopathi <bhoopathi.avt@gmail.com>
2024-04-29 12:59:31 +0530

    Nav bar and Home Page styled

--- Forms-web/src/Components/Navbar.jsx
+++ Forms-web/src/Components/Navbar.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='h-12 flex items-center shadow-sm text-gray-600 justify-around mb-2'>
+      <p>logo</p>
+      <span>FORMS</span>
+      <div className='flex gap-4'>
+        <button className='text-black rounded-sm outline outline-offset-2 outline-1 outline-slate-400 font-medium px-2'>Login</button>
+        <button className=' text-black rounded-sm'>Signup</button>
+      </div>
+    </div>
   )
 }
 
--- Forms-web/src/Pages/Home.jsx
+++ Forms-web/src/Pages/Home.jsx
@@ -5,9 +5,16 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>
-        <Link to={'/form'}>Form</Link>
-        <Link to={'/list'}>List</Link>
+    <div className='w-full w-auto h-lvh flex flex-col gap-10 mt-40 items-center'>
+    <h1 className='font-bold text-4xl'>HANDELING THE FORMS <span className='bg-black text-white p-2 rounded-r-md'>IN DIFF WAY</span></h1>
+    <div className='flex flex-row gap-10'>
+        <div className='shadow-md p-2 w-24 rounded-md flex justify-center items-center h-20 font-semibold transition hover:duration-200 hover:bg-black hover:text-white'>
+          <Link to={'/form'}>Form</Link>
+        </div>
+        <div className='shadow-md p-2 w-24 rounded-md flex justify-center items-center transition hover:duration-200 h-20 font-semibold hover:bg-black hover:text-white'>
+          <Link to={'/list'}>List</Link>
+        </div>
+    </div>
     </div>
   )
 }
