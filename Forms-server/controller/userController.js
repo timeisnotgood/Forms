716fcac7d5744d504cff18f60879897744376b2d
bhoopathi <bhoopathicl@gmail.com>
2024-05-08 21:26:19 +0530

    Signup route completed

--- Forms-server/controller/userController.js
+++ Forms-server/controller/userController.js
@@ -1,23 +1,31 @@
 const expressAsyncHandler = require("express-async-handler")
 const userModel = require("../model/userModel");
 const sequelize = require("../config/db");
+const bcrypt = require("bcrypt")
 
 
 const userSignup = expressAsyncHandler(async(req, res) =>{
 
+    const {id, username, email, password} = req.body;
+    const hashedPassword = await bcrypt.hash(password, 5);
+
+    if (id == null || username == "" || email == null || password == null) {
+        res.status(404)
+        res.json({"Data Not Found" : " Enter the Necessary data "});
+    }
+
     try {
         const insertUser = await userModel.create({
-            id:req.body.id,
-            username:req.body.username,
-            email:req.body.email,
-            password:req.body.password,
+            id:id,
+            username:username,
+            email:email,
+            password:hashedPassword,
         }) 
-        res.status(200).json(insertUser)
         sequelize.close();
+        res.status(200).json(insertUser);
     } catch (error) {
         console.log(error);
     }
-   
 })
 
 module.exports = { userSignup };
