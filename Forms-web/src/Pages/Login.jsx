403adf25821bac4bca068b2c6129054d4a9be2cc
bhoopathi <bhoopathicl@gmail.com>
2024-05-09 15:58:20 +0530

    signIn and Login Feature Added and Completed

--- Forms-web/src/Pages/Login.jsx
+++ Forms-web/src/Pages/Login.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 
 const Login = () => {
+
+  const [logindata, setlogindata] = useState({
+    username : "",
+    password : ""
+  })
+
+  const navigate = useNavigate();
+
+  const inputHandler = (e) =>{
+    setlogindata({...logindata, [e.target.name] : e.target.value});
+  }
+
+  const submitHandler = async(e) =>{
+    e.preventDefault();
+    console.log(logindata);
+
+    const loginResponse = await axios.post(`http://localhost:5000/user/getuser`,{
+      username : logindata.username,
+      password : logindata.password
+    },{
+      Headers : {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'     
+      }
+    }
+  )
+    const res = loginResponse.request.status;
+    console.log(res);
+    if(res == 200) navigate('/')
+
+  }
+
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
 
@@ -10,14 +43,20 @@ const Login = () => {
       <div className='border-solid border rounded-lg w-1/3 h-2/3 shadow-md flex flex-col items-center gap-3 box-border p-5'>
         <h3 className='text-center font-mono font-semibold text-3xl mt-2'>Login</h3>
         <hr className='w-4/5 border-dotted border border-slate-600'/>
-        <form className='flex flex-col w-4/5 h-4/5 gap-6'>
+        <form 
+          onSubmit={submitHandler}
+          className='flex flex-col w-4/5 h-4/5 gap-6'>
           <input 
           type='text' 
           placeholder='Email (or) UserName '
+          name='username'
+          onChange={inputHandler}
           className='border-doted placeholder:italic placeholder:text-slate-400 border mt-3 p-2 rounded-lg'/>
           <input 
-             type='text' 
+            type='text' 
             placeholder='Password'
+            name='password'
+            onChange={inputHandler}
             className='border-doted placeholder:italic placeholder:text-slate-400 border p-2 rounded-lg'
           />
           <button className='border-dashed border-2 rounded-lg px-4 py-2 font-semibold'>Login</button>
--- Forms-web/src/Pages/Signup.jsx
+++ Forms-web/src/Pages/Signup.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+import axios, { Axios } from 'axios';
 
 const Signup = () => {
+
+  const navigate = useNavigate();
+  const [signUpData, setSignUpData] = useState({
+    username : "",
+    email : "",
+    password :""
+  })
+
+  const inputHandler = (e) =>{
+    setSignUpData({...signUpData,[e.target.name] : e.target.value});
+  }
+
+  const submitHandler = async(e) =>{
+    e.preventDefault();
+
+    const signInResponse = await axios.post(`http://localhost:5000/user/postuser`, {
+      id : uuidv4(),
+      username : signUpData.username,
+      email : signUpData.email,
+      password : signUpData.password,
+    },{
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    }})
+
+    const res = signInResponse.request.status;
+    if (res == 200) {
+      navigate("/login")
+    }
+  }
+
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
 
@@ -9,21 +43,30 @@ const Signup = () => {
       <div className='border-solid border rounded-lg w-1/3 h-3/4 shadow-md flex flex-col items-center gap-3 box-border p-5'>
         <h3 className='text-center font-mono font-semibold text-3xl mt-2'>SignUp</h3>
         <hr className='w-4/5 border-dotted border border-slate-600'/>
-        <form className='flex flex-col w-4/5 h-4/5 gap-6'>
+        <form 
+        className='flex flex-col w-4/5 h-4/5 gap-6'
+        onSubmit={submitHandler}>
+        
+          <input 
+          type='text' 
+          placeholder='UserName'
+          name='username'
+          onChange={inputHandler}
+          className='border-doted placeholder:italic placeholder:text-slate-400 border mt-3 p-2 rounded-lg'/>
+
           <input 
           type='text' 
           placeholder='Email'
+          name='email'
+          onChange={inputHandler}
           className='border-doted placeholder:italic placeholder:text-slate-400 border mt-3 p-2 rounded-lg'/>
 
           <input 
-          type='text' 
-          placeholder='UserName '
-          className='border-doted placeholder:italic placeholder:text-slate-400 border mt-3 p-2 rounded-lg'/>
-
-          <input 
-             type='text' 
+            type='text' 
             placeholder='Password'
-            className='border-doted placeholder:italic placeholder:text-slate-400 border p-2 rounded-lg'
+            name='password'
+            onChange={inputHandler}
+            className='border-doted placeholder:italic placeholder:text-slate-400 border mt-3 p-2 rounded-lg'
           />
           <button className='border-dashed border-2 rounded-lg px-4 py-2 font-semibold'>SignUp</button>
   
